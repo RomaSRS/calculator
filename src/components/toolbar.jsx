ae91713a8af4e4fbf343bdd1f3afd2ee2007cccb
romasrs <shaman.roman@tut.by>
2020-02-21 21:56:58 +0300

    feat: add toolbar

---
+++ src/components/toolbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import wrappedHeader from './wrappedheader';
+
+function ToolBar(props) {
+  const { buttonNames, activeButtonIndex, onButtonClick, subClassNames, ...toolbarProps } = props;
+  function createToolButton(buttonName, index) {
+    return (
+      <div
+        className={`tabbutton ${subClassNames}${index === activeButtonIndex ? ' pressed' : ''}`}
+        onClick={event => onButtonClick(event, index)}
+        onKeyPress={event => onButtonClick(event, index)}
+        role="button"
+        tabIndex={0}
+        key={index}
+      >
+        {buttonName}
+      </div>
+    );
+  }
+  const listToolButtons = buttonNames.map(createToolButton);
+  /* eslint-disable */
+  return (
+    <div {...toolbarProps}>
+      {listToolButtons}
+    </div>
+  );
+  /* eslint-enable */
+}
+
+ToolBar.propTypes = {
+  buttonNames: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onButtonClick: PropTypes.func.isRequired,
+  activeButtonIndex: PropTypes.number.isRequired,
+  subClassNames: PropTypes.string,
+};
+
+ToolBar.defaultProps = {
+  subClassNames: '',
+};
+
+const ToolBarWithHeader = wrappedHeader(ToolBar);
+
+export default ToolBarWithHeader;
